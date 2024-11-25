b68d915609008fe43d8d3a8deb7dd6b02fe02cb3
Daniel Mroczek <dmroczek@man.poznan.pl>
2024-11-25 15:54:52 +0100

    Remove Google Analytics integration and related meta tags from HTML

--- analytics.js
+++
@@ -1,6 +0,0 @@
-!function(q,w,e,r,t,y){q.GoogleAnalyticsObject=e;q[e]||(q[e]=function(){
-(q[e].q=q[e].q||[]).push(arguments)});q[e].l=+new Date;t=w.createElement(r);
-y=w.getElementsByTagName(r)[0];t.src='//www.google-analytics.com/analytics.js';
-y.parentNode.insertBefore(t,y)}(window,document,'ga','script');
-ga('create', 'UA-69026072-1', 'auto');
-ga('send', 'pageview');
